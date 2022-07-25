1b2d7518c2f9a2897acccf1a1c00239d38dd11d1
jamespark3922 <jspark96@cs.washington.edu>
2022-07-24 20:31:19 -0700

    add msrvtt contrast set

---
+++ msrvtt/download_msrvtt_train_val.sh
@@ -0,0 +1,8 @@
+# script that saves `train.jsonl` and `val.jsonl` in msrvtt.
+# data based on https://github.com/jayleicn/ClipBERT/blob/main/scripts/download_msrvtt.sh
+
+wget -nc  https://storage.googleapis.com/ai2-jamesp-public/video-contrast-set/msrvtt_retrieval_data.tar.gz -P msrvtt/
+tar -xzf msrvtt/msrvtt_retrieval_data.tar.gz -C msrvtt/
+rm msrvtt/msrvtt_retrieval_data.tar.gz
+
+echo "Extracted 'train.jsonl' and 'val.jsonl' to 'msrvtt' directory!""
